008b1f51cb8cb362acdc90fab96f6779a749cf22
Arina Fonova <fonova99@list.ru>
2024-09-10 17:12:02 +0300

    Saving and loading data from a file

---
+++ src/Lab5/Main.java
@@ -0,0 +1,7 @@
+package Lab5;
+
+public class Main {
+    public static void main(String[] args) {
+        new Lab5().show();
+    }
+}
---
+++ src/Lab5/TextException.java
@@ -0,0 +1,14 @@
+package Lab5;
+
+import javax.swing.*;
+
+public class TextException extends  Exception{
+    public TextException() {
+        super("Вы не ввели ФИО или должность доктора");
+    }
+    public void checkException(JTextField doctor) throws TextException, NullPointerException{
+        String str = doctor.getText();
+        if (str.contains("ФИО или должность доктора")) throw new TextException();
+        if (str.length() == 0) throw new NullPointerException();
+    }
+}
