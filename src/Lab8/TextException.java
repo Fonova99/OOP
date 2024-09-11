b4f75ccbe987c2f21fa166e34eb9c4db850ee1c0
Arina Fonova <fonova99@list.ru>
2024-09-11 22:44:16 +0300

    Creating multithreading

---
+++ src/Lab8/TextException.java
@@ -0,0 +1,15 @@
+package Lab8;
+
+import javax.swing.*;
+
+public class TextException extends  Exception{
+    public TextException() {
+        super("Вы не ввели название заболевания");
+    }
+    public void checkException(JTextField disease) throws TextException, NullPointerException{
+        String str = disease.getText();
+        if (str.contains("Введите название заболевания")) throw new TextException();
+        if (str.length() == 0) throw new NullPointerException();
+    }
+}
+
